cc0f538a1a5b101f5ffcc9d5bf13dae449258df3
ChiWon2 <ckw0655@gmail.com>
2026-06-06 11:09:13 +0900

    [Main Page] Add ReasonSection

--- js/main.js
+++ js/main.js
@@ -1,6 +1,37 @@
 const mainPlant = document.querySelector("#mainPlant");
 const gardenPlants = document.querySelectorAll(".garden-scene .plant");
 const gardenScene = document.querySelector(".garden-scene");
+// sections except heroSection
+const points = document.querySelectorAll(".snap-point");
+
+let currentIndex = -1;
+let isScrolling = false;
+
+window.addEventListener("wheel", (e) => {
+  if (isScrolling) return;
+
+  isScrolling = true;
+
+  if (e.deltaY > 0) 
+  {
+    currentIndex = Math.min(currentIndex + 1,points.length - 1);
+  } 
+  else
+  {
+    currentIndex = Math.max(currentIndex - 1,-1);
+  }
+
+  if (currentIndex === -1) 
+  {
+    window.scrollTo({top: 0, behavior: "smooth"});
+  } 
+  else 
+  {
+    points[currentIndex].scrollIntoView({behavior: "smooth", block: "start"});
+  }
+
+  setTimeout(() => { isScrolling = false;}, 200);
+});
 
 //TODO CHECK :: can i delete this??
 const plantClasses = [
